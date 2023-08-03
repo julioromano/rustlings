bc055330aa49da764deaa0ac1465db979e99ec72
Marco Romano <marcojulioromano@gmail.com>
2023-08-03 23:30:23 +0200

    First chunk of exercises.

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(no_of_apples: i32) -> i32 {
+    if no_of_apples <= 40 {
+        no_of_apples * 2
+    } else {
+        no_of_apples
+    }
+}
 
 // Don't modify this function!
 #[test]
